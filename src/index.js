4a6c784f234b8daa1c14d9363f186dd685870831
omar-faruk72 <mdfaruk01720875872@gmail.com>
2026-03-01 10:51:17 +0600

    env file config /index.js file uptimaize

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import express from 'express'; // require এর বদলে import
-import dotenv from 'dotenv';
+import express from 'express'; 
 import cors from 'cors';
+import config from './config/index.js';
 
-dotenv.config();
+
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = config.port;
 
 // Middleware
 app.use(cors());
@@ -15,6 +15,8 @@ app.get('/', (req, res) => {
     res.send('Zero Framework Server is Running with ESM!');
 });
 
+
+
 app.listen(port, () => {
-    console.log(`🚀 Server is running on http://localhost:${port}`);
+    console.log(` Server is running on http://localhost:${port}`);
 });
