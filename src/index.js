19d9b4ea0c7b2b98dd9bb846f803376e073ab85e
omar-faruk72 <mdfaruk01720875872@gmail.com>
2026-03-02 13:31:24 +0600

    added an cretate user api complite

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import config from './config/index.js';
 import { connectDB } from './config/db.js';
 import { dataPostRoutes } from './modules/data-post/data-post.routes.js';
+import { userRouter } from './modules/user/user.routes.js';
 
 const app = express();
 const port = config.port;
@@ -15,6 +16,9 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Zero Framework Server is Running with ESM!');
 });
+
+// user
+app.use('/users', userRouter);
 
 app.use('/post', dataPostRoutes)
 
---
+++ src/modules/user/user.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { userControllers } from "./user.controller";
+
+const router = Router();
+
+router.post('', userControllers.createUser);
+
+
+export const userRouter = router;
