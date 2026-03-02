8c94a54cbd909d7545704e5d64d61fb46ab5d0e1
omar-faruk72 <mdfaruk01720875872@gmail.com>
2026-03-02 13:59:44 +0600

    added an register api successfully created and password hasd

--- src/modules/user/user.service.js
+++ src/modules/user/user.service.js
@@ -1,17 +1,30 @@
-import collections from "../../config/collections.js"
-
+import collections from "../../config/collections.js";
+import bcrypt from 'bcryptjs';
 const createUser = async(userData) => {
     const email = userData.email;
+    const password = userData.password;
     try{
         const existingUser = await collections.usersCollection.findOne({email: email});
-
+        // duplicate use not allawortd
         if(existingUser) {
             const error = new Error("This email is already registered!");
             error.statusCode = 400;
             throw error;
         };
 
-        const result = await collections.usersCollection.insertOne(userData);
+        // password hash
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // new user
+        const newUser = {
+            ...userData,
+            password: hashedPassword,
+            created_at: new Date(),
+            role: 'user'
+        }
+
+        const result = await collections.usersCollection.insertOne(newUser);
         return result;
     }catch(err) {
         throw err;
